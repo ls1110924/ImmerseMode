e45e0363a9b173053316b4a32c9d04d865cd25d1
A Shuai <ls1110924@gmail.com>
2017-01-27 00:16:24 +0800

    complete normal immerse mode statusbar test code

--- app/src/main/java/com/yunxian/immersemode/sample/NormalActivity.java
+++ app/src/main/java/com/yunxian/immersemode/sample/NormalActivity.java
@@ -2,7 +2,13 @@ package com.yunxian.immersemode.sample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.yunxian.immerse.ImmerseHelper;
+import com.yunxian.immerse.enumeration.StatusBarImmerseType;
 
 /**
  * @author AShuai
@@ -14,5 +20,26 @@ public class NormalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_normal);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+
+        ImmerseHelper immerseHelper = new ImmerseHelper(this, StatusBarImmerseType.NORMAL, StatusBarImmerseType.NORMAL);
+        immerseHelper.setStatusColorRes(R.color.colorAccent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
