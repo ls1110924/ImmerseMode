dac34f91af355d556bbb7ad287256815d2836886
A Shuai <ls1110924@gmail.com>
2017-01-27 22:49:03 +0800

    add translucent full screen mode test code

--- app/src/main/java/com/yunxian/immersemode/sample/TranslucentFullActivity.java
+++ app/src/main/java/com/yunxian/immersemode/sample/TranslucentFullActivity.java
@@ -6,10 +6,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import com.yunxian.immerse.ImmerseHelper;
 import com.yunxian.immerse.enumeration.StatusBarImmerseType;
+import com.yunxian.immersemode.sample.widget.ExScrollView;
 
 /**
  * @author AShuai
@@ -18,20 +23,39 @@ import com.yunxian.immerse.enumeration.StatusBarImmerseType;
  */
 public class TranslucentFullActivity extends AppCompatActivity {
 
+    private CommonCallbackListener mCommonListener = new CommonCallbackListener();
+
+    private Toolbar mToolbar;
+    private ImageView mImgHeader;
+    private ImmerseHelper mImmerseHelper;
+
+    private int mRed;
+    private int mGreen;
+    private int mBlue;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mRed = 0xFF;
+        mGreen = 0x40;
+        mBlue = 0x81;
+
         setContentView(R.layout.activity_translucent_full);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setBackgroundColor(Color.TRANSPARENT);
+        mToolbar.setBackgroundColor(Color.TRANSPARENT);
 
-        ImmerseHelper immerseHelper = new ImmerseHelper(this, StatusBarImmerseType.TRANSLUCENT,
+        mImgHeader = (ImageView) findViewById(R.id.img_header);
+
+        mImmerseHelper = new ImmerseHelper(this, StatusBarImmerseType.TRANSLUCENT,
                 StatusBarImmerseType.TRANSLUCENT, true);
-        immerseHelper.setStatusColor(Color.TRANSPARENT);
+        mImmerseHelper.setStatusColor(Color.TRANSPARENT);
+
+        ExScrollView scrollView = (ExScrollView) findViewById(R.id.scroll_view);
+        scrollView.setOnScrollChangeListener(mCommonListener);
     }
 
     @Override
@@ -43,6 +67,30 @@ public class TranslucentFullActivity extends AppCompatActivity {
             return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    private int imgHeadHeight = 0;
+
+    private class CommonCallbackListener implements ExScrollView.OnScrollChangeListener {
+        @Override
+        public void onScrollChanged(int l, int t, int oldl, int oldt) {
+            if (imgHeadHeight == 0) {
+                imgHeadHeight = mImgHeader.getHeight();
+            }
+            if (imgHeadHeight == 0) {
+                return;
+            }
+            if (t < 0) {
+                t = 0;
+            }
+            if (t > imgHeadHeight) {
+                t = imgHeadHeight;
+            }
+            float alphaF = (float) t / imgHeadHeight * 255;
+            int color = Color.argb((int) alphaF, mRed, mGreen, mBlue);
+            mImmerseHelper.setStatusColor(color);
+            mToolbar.setBackgroundColor(color);
+        }
     }
 
 }
---
+++ app/src/main/java/com/yunxian/immersemode/sample/widget/ExScrollView.java
@@ -0,0 +1,45 @@
+package com.yunxian.immersemode.sample.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * @author AShuai
+ * @email lishuai.ls@alibaba-inc.com
+ * @date 17/1/27 下午10:01
+ */
+public class ExScrollView extends ScrollView {
+
+    private OnScrollChangeListener mOnScrollChangeListener;
+
+    public ExScrollView(Context context) {
+        super(context);
+    }
+
+    public ExScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setOnScrollChangeListener(OnScrollChangeListener onScrollChangeListener) {
+        mOnScrollChangeListener = onScrollChangeListener;
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+
+        if (mOnScrollChangeListener != null) {
+            mOnScrollChangeListener.onScrollChanged(l, t, oldl, oldt);
+        }
+    }
+
+    public interface OnScrollChangeListener {
+        void onScrollChanged(int l, int t, int oldl, int oldt);
+    }
+
+}
