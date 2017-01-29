3fe1f982fdc31c72406db14e62e7c2f20c441963
A Shuai <ls1110924@gmail.com>
2017-01-29 14:05:43 +0800

    add consume insets setter method for consume insets frame layout

--- immerse/src/main/java/com/yunxian/immerse/widget/ConsumeInsetsFrameLayout.java
+++ immerse/src/main/java/com/yunxian/immerse/widget/ConsumeInsetsFrameLayout.java
@@ -75,6 +75,24 @@ public class ConsumeInsetsFrameLayout extends FrameLayout {
         return super.fitSystemWindows(insets);
     }
 
+    /**
+     * 设置是否消费Insets事件。
+     * <p>沉浸Activity中包含EditText时，请设置为true，否则为false</p>
+     * <p>一个Activity只包含一个fitSystemWindows为ture的控件，因此别的控件再行设置无效。
+     * 需使用{@link OnInsetsChangeListener}进行监听</p>
+     *
+     * @param consumeInsets true为消费，否则不消费
+     */
+    public void setConsumeInsets(boolean consumeInsets) {
+        if (mConsumeInsets != consumeInsets) {
+            mConsumeInsets = consumeInsets;
+            if (SDK_INT >= KITKAT) {
+                setFitsSystemWindows(consumeInsets);
+            }
+            requestLayout();
+        }
+    }
+
     public void addOnInsetsChangeListener(@Nullable OnInsetsChangeListener listener) {
         if (listener != null && !mOnInsetsChangeListeners.contains(listener)) {
             mOnInsetsChangeListeners.add(listener);
