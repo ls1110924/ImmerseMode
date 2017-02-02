bbfc007c32c1ea727f25f61df0d3245be8ebd21c
A Shuai <ls1110924@gmail.com>
2017-02-02 16:37:10 +0800

    add activityConfig and optimize translucent statusbar navigationbar mode

--- immerse/src/main/java/com/yunxian/immerse/impl/TlSbTlNbImmerseMode.java
+++ immerse/src/main/java/com/yunxian/immerse/impl/TlSbTlNbImmerseMode.java
@@ -9,11 +9,14 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.FrameLayout;
 
 import com.yunxian.immerse.IImmerseMode;
+import com.yunxian.immerse.manager.ActivityConfig;
 import com.yunxian.immerse.manager.GlobalConfig;
 import com.yunxian.immerse.utils.DrawableUtils;
 import com.yunxian.immerse.utils.WindowUtils;
@@ -21,9 +24,7 @@ import com.yunxian.immerse.widget.ConsumeInsetsFrameLayout;
 
 import java.lang.ref.SoftReference;
 
-import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.KITKAT;
-import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
 import static android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
 
@@ -36,6 +37,8 @@ import static android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
 public class TlSbTlNbImmerseMode implements IImmerseMode {
 
     private final SoftReference<Activity> mActivityRef;
+
+    private final ActivityConfig mActivityConfig;
 
     private final View mStatusBarView;
 
@@ -50,6 +53,7 @@ public class TlSbTlNbImmerseMode implements IImmerseMode {
         WindowUtils.addWindowFlags(window, FLAG_TRANSLUCENT_STATUS);
         WindowUtils.addWindowFlags(window, FLAG_TRANSLUCENT_NAVIGATION);
 
+        mActivityConfig = new ActivityConfig(activity);
         mStatusBarView = setupStatusBarView(activity);
         mNavigationBarView = setupNavigationBarView(activity);
     }
@@ -142,13 +146,26 @@ public class TlSbTlNbImmerseMode implements IImmerseMode {
 
     @Nullable
     private View setupNavigationBarView(@NonNull Activity activity) throws IllegalStateException {
-        ViewGroup contentViewGroup = (ViewGroup) activity.findViewById(android.R.id.content);
-        View userView = contentViewGroup.getChildAt(0);
-        if (userView == null) {
-            throw new IllegalStateException("Plz invode setContentView() method first!");
-        }
+        View navigationBarView = null;
+        if (mActivityConfig.hasNavigtionBar()) {
+            ViewGroup contentViewGroup = (ViewGroup) activity.findViewById(android.R.id.content);
+            View userView = contentViewGroup.getChildAt(0);
+            if (userView == null) {
+                throw new IllegalStateException("Plz invode setContentView() method first!");
+            }
 
-        return null;
+            navigationBarView = new View(activity);
+            FrameLayout.LayoutParams params;
+            if (mActivityConfig.isNavigationAtBottom()) {
+                params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, mActivityConfig.getNavigationBarHeight());
+                params.gravity = Gravity.BOTTOM;
+            } else {
+                params = new FrameLayout.LayoutParams(mActivityConfig.getNavigationBarWidth(), FrameLayout.LayoutParams.MATCH_PARENT);
+                params.gravity = Gravity.RIGHT;
+            }
+            contentViewGroup.addView(navigationBarView, params);
+        }
+        return navigationBarView;
     }
 
 }
--- immerse/src/main/java/com/yunxian/immerse/manager/ActivityConfig.java
+++ immerse/src/main/java/com/yunxian/immerse/manager/ActivityConfig.java
@@ -1,12 +1,144 @@
 package com.yunxian.immerse.manager;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+import android.view.ViewConfiguration;
+
+import com.yunxian.immerse.utils.ResourcesUtils;
+
+import java.lang.reflect.Method;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+import static android.os.Build.VERSION_CODES.KITKAT;
+
 /**
+ * 每一个Activity页面自己特有的配置信息包装类
+ *
  * @author AShuai
  * @email ls1110924@gmail.com
  * @date 17/1/31 下午8:28
  */
 public class ActivityConfig {
 
-//    private final
+    private static String sNavBarOverride;
+
+    static {
+        // Android allows a system property to override the presence of the navigation bar.
+        // Used by the emulator.
+        // See https://github.com/android/platform_frameworks_base/blob/master/policy/src/com/android/internal/policy/impl/PhoneWindowManager.java#L1076
+        if (SDK_INT >= KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+                sNavBarOverride = null;
+            }
+        }
+    }
+
+    private static final String NAV_BAR_HEIGHT_RES_NAME = "navigation_bar_height";
+    private static final String NAV_BAR_HEIGHT_LANDSCAPE_RES_NAME = "navigation_bar_height_landscape";
+    private static final String NAV_BAR_WIDTH_RES_NAME = "navigation_bar_width";
+    private static final String SHOW_NAV_BAR_RES_NAME = "config_showNavigationBar";
+
+
+    private final boolean mInPortrait;
+    private final float mSmallestWidthDp;
+    private final boolean mHasNavigationBar;
+    private final int mNavigationBarHeight;
+    private final int mNavigationBarWidth;
+
+    public ActivityConfig(@NonNull Activity activity) {
+        Resources res = activity.getResources();
+        mInPortrait = (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
+        mSmallestWidthDp = getSmallestWidthDp(activity);
+        mNavigationBarHeight = getNavigationBarHeight(activity);
+        mNavigationBarWidth = getNavigationBarWidth(activity);
+        mHasNavigationBar = mNavigationBarHeight > 0;
+    }
+
+    public boolean hasNavigtionBar() {
+        return mHasNavigationBar;
+    }
+
+    public boolean isNavigationAtBottom() {
+        return (mSmallestWidthDp >= 600 || mInPortrait);
+    }
+
+    public int getNavigationBarHeight() {
+        return mNavigationBarHeight;
+    }
+
+    public int getNavigationBarWidth() {
+        return mNavigationBarWidth;
+    }
+
+    private float getSmallestWidthDp(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+        } else {
+            // TODO this is not correct, but we don't really care pre-kitkat
+            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        }
+        float widthDp = metrics.widthPixels / metrics.density;
+        float heightDp = metrics.heightPixels / metrics.density;
+        return Math.min(widthDp, heightDp);
+    }
+
+    private int getNavigationBarHeight(Context context) {
+        Resources res = context.getResources();
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context)) {
+                String key;
+                if (mInPortrait) {
+                    key = NAV_BAR_HEIGHT_RES_NAME;
+                } else {
+                    key = NAV_BAR_HEIGHT_LANDSCAPE_RES_NAME;
+                }
+                return ResourcesUtils.getDimensionSize(res, key, "android");
+            }
+        }
+        return result;
+    }
+
+    private int getNavigationBarWidth(Context context) {
+        Resources res = context.getResources();
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context)) {
+                return ResourcesUtils.getDimensionSize(res, NAV_BAR_WIDTH_RES_NAME, "android");
+            }
+        }
+        return result;
+    }
+
+    @TargetApi(ICE_CREAM_SANDWICH)
+    private boolean hasNavBar(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier(SHOW_NAV_BAR_RES_NAME, "bool", "android");
+        if (resourceId != 0) {
+            boolean hasNav = res.getBoolean(resourceId);
+            // check override flag (see static block)
+            if ("1".equals(sNavBarOverride)) {
+                hasNav = false;
+            } else if ("0".equals(sNavBarOverride)) {
+                hasNav = true;
+            }
+            return hasNav;
+        } else { // fallback
+            return !ViewConfiguration.get(context).hasPermanentMenuKey();
+        }
+    }
 
 }
